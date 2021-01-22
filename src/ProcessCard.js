b1edd3633690f24edce921bba175822bfb452888
ejnn <eduardojorgenicolau@gmail.com>
2021-01-22 17:10:04 -0300

    ajustes de UX no ProcessCard

--- src/ProcessCard.js
+++ src/ProcessCard.js
@@ -1,80 +1,107 @@
 import { Grid } from "@material-ui/core";
 import { Card, CardContent } from "@material-ui/core";
 import { Typography } from "@material-ui/core";
-import image from "./imagemCardPequena.svg";
+import { Box } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-const normalCardRecipe = {
-    entries: {
-	"Número": "numero",
-	"Assunto": "assunto",
-	"Interessado": "interessado",
-	"Descrição": "descricao",
+import theme from "./theme.js";
+import cardImage from "./imagemProcesso.svg";
+
+const useStyles = makeStyles({
+    ...theme,
+    
+    body: {
+	...theme.body,
+	color: theme.colors.black87,
+	textTransform: "none",
     },
-    xs: 3,
-}
 
-const shortenedCardRecipe = {
-    entries: {
-	"Número": "numero",
-	"Assunto": "assunto",
-	"Interessado": "interessado",
+    subtitle: {
+	...theme.subtitle,
+	color: theme.colors.black54,
+	textTransform: "none",
     },
-    xs: 6,
-}
 
-const createCardContents = (recipe, process) =>
-<Grid item
-      container
-      alignItems="center"
-      spacing={1}
-      xs={10}>
-    {
-    	Object.keys(recipe.entries)
-	    .map(key => 
-		<Grid item
-		      xs={recipe.xs}
-		      key={key}>
-    		    <Typography variant="subtitle1">
-    			{key}
-    		    </Typography>
-		</Grid>
-	    )
-    }
-    {
-    	Object.values(recipe.entries)
-	    .map(value => 
-		<Grid item
-		      xs={recipe.xs}
-		      key={value}>
-    		    <Typography variant="body1"
-    				noWrap>
-    			{process[value]}
-    		    </Typography>
-		</Grid>
-	    )
-    }
-</Grid>;
+    highlightedText: {
+	color: theme.colors.primary,
+    },
 
-const ProcessCard = ({ process, shortened }) => {
-      return (
-	  <Card>
-	      <CardContent>
-		  <Grid container
-			alignItems="center"
-			spacing={1}>
-		      { shortened ? null
-			:
-			<Grid item
-			      xs="auto">
-			    <img src={image}/>
+    highlightedCard: {
+	outline: "1px solid " + theme.colors.primary,
+    },
+    
+    processCardContents: {
+	padding: 16,
+	
+	"&:last-child": {
+	    paddingBottom: 16,
+	},
+    },
+
+    widthfulImage: {
+	width: "100%",
+    },
+    
+});
+
+const ProcessCard = ({ process, shortened, highlighted }) => {
+    const classes = useStyles();
+
+    const createTypographyCombo = (title, body) => {
+	return (
+	    <>
+		<Typography className={`${classes.subtitle} ${highlighted ? classes.highlightedText : ""}`}>
+		    {title}
+		</Typography>
+		<Typography noWrap
+			    className={`${classes.body} ${highlighted ? classes.highlightedText : ""}`}>
+		    {body}
+		</Typography>
+	    </>
+	);
+    };
+    
+    return (
+	<Card className={`${highlighted ? classes.highlightedCard : ""}`}>
+	    <CardContent className={classes.processCardContents}>
+		<Grid container
+		      spacing={1}
+		      alignItems="center">
+		    { shortened ? null
+		      : <Grid item
+			      xs={2}>
+			    <Box display="flex">
+				<img className={classes.widthfulImage}
+				     src={cardImage}/>
+			    </Box>
 			</Grid>
-		      }
-		  {
-		      createCardContents(
-			  shortened ? shortenedCardRecipe :normalCardRecipe,
-			  {...process, interessado: process["interessados"][0]})
-		  }
+		    }
+		    <Grid item
+			  container
+			  spacing={1}
+			  alignItems="center"
+			  xs={10}>
+			<Grid item
+			      xs={shortened ? 6 : 3}>
+			    { createTypographyCombo("Número", process.numero) }
+			</Grid>
+			<Grid item
+			      xs={shortened ? 6 : 3}>
+			    { createTypographyCombo("Assunto", process.assunto) }
+			</Grid>
+			<Grid item
+			      xs={shortened ? 6 : 3}>
+			    { createTypographyCombo("Interessado", process.interessados[0]) }
+			</Grid>
+			{ shortened ? null
+			  :
+			  <Grid item
+				xs={3}>
+			      { createTypographyCombo("Descrição", process.descricao) }
+			  </Grid>
+			}
 		      </Grid>
+		  </Grid>
 	      </CardContent>
 	  </Card>
       );
---
+++ src/theme.js
@@ -0,0 +1,42 @@
+import "@fontsource/montserrat";
+
+const theme = {
+    headline: {
+	"font-family": "Montserrat",
+	"font-size": "24px",
+	"font-weight": "normal",
+    },
+
+    title: {
+	"font-family": "Montserrat",
+	"font-size": "20px",
+	"font-weight": "normal",
+    },
+
+    subtitle: {
+	"font-family": "Montserrat",
+	"font-size": "14px",
+	"font-weight": "bold",
+    },
+
+    body: {
+	"font-family": "Montserrat",
+	"font-size": "16px",
+	"font-weight": "normal",
+    },
+
+    buttonLabel: {
+	"font-family": "Montserrat",
+	"font-size": "14px",
+	"font-weight": "bold",
+    },
+
+    colors: {
+	"primary": "#005b85",
+	"black87": "#212121",
+	"black54": "#757575",
+	"black38": "#9E9E9E",
+    },
+};
+
+export default theme;
